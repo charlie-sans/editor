e75a7c0dd6e237a0fe0939b654bef3b5cc108e5f
charlie-sans <gamerboy030607@gmail.com>
2024-07-24 22:48:38 +1000

    :3

--- src/main/java/org/charlie/languageServer.java
+++ src/main/java/org/charlie/languageServer.java
@@ -1,7 +1,7 @@
 package org.charlie;
 import org.charlie.Logging;
 public class languageServer {
-    public static server() {
+    public static void server() {
         Logging.print("server started",Main.DEBUG);
     }
 }
